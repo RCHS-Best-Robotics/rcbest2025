9645e23bfedc29a1d174399b1ba2e42518570864
dexteram <aidenmdexter@gmail.com>
2025-09-20 12:58:15 -0600

    created base files and included dependencies again

---
+++ robotcodepy/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       aiden                                                        #
+# 	Created:      9/20/2025, 12:57:51 PM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
