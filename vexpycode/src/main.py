2c505664ac9e2c2b0301ed1cb00d59be7615589b
dexteram <aidenmdexter@gmail.com>
2025-09-20 12:43:42 -0600

    added base vex code

---
+++ vexpycode/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       aiden                                                        #
+# 	Created:      9/20/2025, 12:42:33 PM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
