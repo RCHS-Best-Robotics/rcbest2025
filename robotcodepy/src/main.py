f258613ce86eb7308a2736c5d46d8218a674af06
dexteram <aidenmdexter@gmail.com>
2025-10-06 16:19:43 -0600

    the motors move now frfr

--- robotcodepy/src/main.py
+++ robotcodepy/src/main.py
@@ -13,59 +13,43 @@ from vex import *
 # Brain should be defined by default
 brain=Brain()
 controller = Controller()
-left_motor = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
-right_motor = Motor(Ports.PORT3, GearSetting.RATIO_18_1, True)
-left_motor.set_velocity(100, PERCENT)
-right_motor.set_velocity(100, PERCENT)
+left_motor = Motor(Ports.PORT5, GearSetting.RATIO_18_1, True)
+right_motor = Motor(Ports.PORT4, GearSetting.RATIO_18_1, True)
 left_motor.spin(DirectionType.FORWARD)
 right_motor.spin(DirectionType.FORWARD)
-wait(2, SECONDS)
-left_motor.stop()
-right_motor.stop()
+
 test = False
 run = True
 
 while run == True:
-    posxR = controller.axis1.position()
-    posyR = controller.axis2.position()
-    posyL = controller.axis3.position()
-    posxL = controller.axis4.position()
-    VfwdL = posyL
-    VfwdR = posyL
+    pos1 = int(controller.axis1.position())
+    pos2 = int(controller.axis2.position())
+    pos3 = int(controller.axis3.position())
+    pos4 = int(controller.axis4.position())
+    VfwdL = pos3
+    VfwdR = -(pos3)
+
     
-    if(posxL>0):
-        VfwdL = VfwdL-abs(posxL)
-    else:
-        VfwdR = VfwdR-abs(posxL)
     left_motor.set_velocity(VfwdL, PERCENT)
     right_motor.set_velocity(VfwdR, PERCENT)
 
-    if(posyL<0):
-        left_motor.spin(DirectionType.REVERSE)
-        right_motor.spin(DirectionType.REVERSE)
-    else:
-        left_motor.spin(DirectionType.FORWARD)
-        right_motor.spin(DirectionType.FORWARD)
+    brain.screen.print("1: ",pos1,"; 2: ",pos2)
+    brain.screen.print("; 4: ",pos4,"; 3: ",pos3)
+    brain.screen.clear_row(1)
+    brain.screen.set_cursor(1,1)
     
 
 
 
 while test == True:
-    posxR = controller.axis1.position()
-    posyR = controller.axis2.position()
-    posyL = controller.axis3.position()
-    posxL = controller.axis4.position()
-    brain.screen.print("xR: ",posxR,"; yR: ",posyR)
-    brain.screen.print("; xL: ",posxL,"; yL: ",posyL)
+    pos1 = controller.axis1.position()
+    pos2 = controller.axis2.position()
+    pos3 = controller.axis3.position()
+    pos4 = controller.axis4.position()
+    brain.screen.print("1: ",pos1,"; 2: ",pos2)
+    brain.screen.print("; 4: ",pos4,"; 3: ",pos3)
     wait(2000, MSEC)
     brain.screen.clear_row(1)
     brain.screen.set_cursor(1,1)
     wait(20, MSEC)
 
-
-
-
-        
-
-def map_input(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
