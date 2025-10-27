2882f33dcaf2ab441310c2cf47a414cde7a8b3dc
dexteram <aidenmdexter@gmail.com>
2025-10-27 15:45:11 -0600

    fixed turning

--- robotcodepy/src/main.py
+++ robotcodepy/src/main.py
@@ -16,7 +16,7 @@ controller = Controller()
 left_motor = Motor(Ports.PORT5, GearSetting.RATIO_18_1, True)
 right_motor = Motor(Ports.PORT6, GearSetting.RATIO_18_1, True)
 big_arm_motor = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
-small_arm_motor = Motor(Ports.PORT9, GearSetting.RATIO_18_1, True)
+small_arm_motor = Motor(Ports.PORT7, GearSetting.RATIO_18_1, True)
 
 left_motor.spin(DirectionType.FORWARD)
 right_motor.spin(DirectionType.FORWARD)
@@ -29,25 +29,38 @@ servoSL = Servo(brain.three_wire_port.e)
 servoSR = Servo(brain.three_wire_port.g)
 
 def big_arm_grab():
-    print("big arm grab")
     servoBL.set_position(-43, DEGREES)
     servoBR.set_position(12, DEGREES)
 
 def big_arm_release():
-    print("big arm release")
     servoBL.set_position(20, DEGREES)
     servoBR.set_position(-50, DEGREES)
 
 def small_arm_grab():
-    print("small arm grab")
+    servoSL.set_position(-20, DEGREES)
+    servoSR.set_position(20, DEGREES)    
 
 def small_arm_release():
-    print("small arm release")
+    servoSL.set_position(90, DEGREES)
+    servoSR.set_position(-90, DEGREES)
+
+def default_pos():
+    servoBL.set_position(20, DEGREES)
+    servoBR.set_position(-50, DEGREES)
+    servoSL.set_position(90, DEGREES)
+    servoSR.set_position(-90, DEGREES)
+    
+def comp_pos():
+    servoBL.set_position(-43, DEGREES)
+    servoBR.set_position(12, DEGREES)
+    servoSL.set_position(-20, DEGREES)
+    servoSR.set_position(20, DEGREES)    
 
 
 test = False
 run = True
 
+default_pos()
 
 while run == True:
     pos1 = int(controller.axis1.position())
@@ -57,7 +70,7 @@ while run == True:
     VfwdL = pos3
     VfwdR = -pos3
 
-    if (pos4>40)or(pos4<-40):
+    if (pos4<20)and(pos4>-20):
         VfwdR = pos3 + pos4
         VfwdL = pos3 + pos4
 
@@ -66,8 +79,13 @@ while run == True:
     left_motor.set_velocity(VfwdL, PERCENT)
     right_motor.set_velocity(VfwdR, PERCENT)
 
-    if(pos1 == 0):
+    if ((pos1<20)and(pos1>-20))and((pos2>20)or(pos2<-20)):
         big_arm_motor.set_velocity(pos2, PERCENT)
+    
+    else:
+        small_arm_motor.set_velocity(pos1, PERCENT)
+    
+
 
     if(controller.buttonR1.pressing()):
         big_arm_grab()
@@ -78,14 +96,20 @@ while run == True:
     if(controller.buttonL1.pressing()):
         small_arm_grab()
     
-    if(controller.buttonL1.pressing()):
+    if(controller.buttonL2.pressing()):
         small_arm_release()
 
-    #brain.screen.print("1: ",pos1,"; 2: ",pos2)
-    #brain.screen.print("; 4: ",pos4,"; 3: ",pos3)
-    #brain.screen.clear_row(1)
-    #brain.screen.set_cursor(1,1)
-    
+
+    if(controller.buttonA.pressing()):
+        default_pos()
+
+    if(controller.buttonB.pressing()):
+        comp_pos()
+
+    left_motor.spin(DirectionType.FORWARD)
+    right_motor.spin(DirectionType.FORWARD)
+    big_arm_motor.spin(DirectionType.FORWARD)
+    small_arm_motor.spin(DirectionType.FORWARD)
 
 
 
